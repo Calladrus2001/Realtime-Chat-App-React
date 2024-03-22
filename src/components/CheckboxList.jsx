40fa69b95ba1ac3d04af319191e034225eca73ee
Calladrus2001 <dugarvishesh@gmail.com>
2024-03-22 20:36:17 +0530

    refactor: CheckboxList for more reusability

--- src/components/CheckboxList.jsx
+++ src/components/CheckboxList.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { Checkbox } from "./ui/checkbox";
-function CheckboxList({ contacts, setselectedContacts }) {
-  const onCheckboxChange = (contactId) => {
-    setselectedContacts((prevSelectedContacts) => {
-      // Check if the contactId is already in the selectedContacts array
-      const contactIndex = prevSelectedContacts.indexOf(contactId);
-      if (contactIndex === -1) {
+function CheckboxList({items, setItems, filterAttribute, displayAttribute}) {
+  const onCheckboxChange = (item) => {
+    setItems((prevItems) => {
+      // Check if the item is already in the array
+      const itemIndex = prevItems.indexOf(item);
+      if (itemIndex === -1) {
         // If not found, add it to the array
-        return [...prevSelectedContacts, contactId];
+        return [...prevItems, item];
       } else {
         // If found, remove it from the array
-        return prevSelectedContacts.filter((id) => id !== contactId);
+        return prevItems.filter((item) => item !== item);
       }
     });
   };
 
   return (
     <ul>
-      {contacts.map((contact) => (
-        <li key={contact.contact_id}>
-          <Checkbox onClick={() => onCheckboxChange(contact.contact_id)} />
-          {contact.contact_email}
+      {items.map((item) => (
+        <li key={item[`${filterAttribute}`]}>
+          <Checkbox onClick={() => onCheckboxChange(item[`${filterAttribute}`])} />
+          {item[`${displayAttribute}`]}
         </li>
       ))}
     </ul>
